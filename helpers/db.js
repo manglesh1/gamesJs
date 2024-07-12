965faf8b897dd1f4076687aaff5ba419a2640470
ErAshishChawla <ashishchawladev@gmail.com>
2024-07-12 10:31:42 -0400

    Trusted server certificate

--- helpers/db.js
+++ helpers/db.js
@@ -4,6 +4,12 @@ const db = new Sequelize("games", "admin", "Aero@password1", {
   host: "192.186.105.194",
   port: "1433",
   dialect: "mssql",
+  dialectOptions: {
+    options: {
+      encrypt: true,
+      trustServerCertificate: true,
+    },
+  },
 });
 
 module.exports = {
